d73b117371bdc7cbb2012e89fc49b662214fd9ff
Enrico Zamprogno <enricozamprogno@users.noreply.github.com>
2014-05-07 14:48:54 +0200

    static tables to decode fiscal code

--- listacomuni.php
+++ listacomuni.php
@@ -1,4 +1,7 @@
 <?php
+
+/* static array of data to decode codice fiscale */
+
 $tab_mesi=array('A' => '01', 'B'=> '02', 'C' => '03',  'D' => '04', 'E' => '05', 'H' => '06',	'L' => '07','M' => '08', 'P' => '09', 'R' => '10', 'S' => '11', 'T'=>'12' );
 
 // `cf`.`comuni`
